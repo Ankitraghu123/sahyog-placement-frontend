d5effbc9267743e29833da1cb04f16a8d5a344a9
Devanshi Bilthare <100258554+Devanshi-Bilthare@users.noreply.github.com>
2024-09-14 13:37:51 +0530

    added dashboard link

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -104,7 +104,7 @@ const Footer = () => {
                         <a href="mailto:info@sahyogplacement.com">info@sahyogplacement.com</a>
                       </li>
                       <button className="btn text-center bg-white px-4 mt-3" >
-                        <a style={{ color: '#462B34'}} href="https://sahyog-placement-dashboard-beta.vercel.app/#/">A Login</a>
+                        <a style={{ color: '#462B34'}} href="https://sahyogplacement.vercel.app/#/">A Login</a>
                       </button>
                     </ul>
                   </div>
