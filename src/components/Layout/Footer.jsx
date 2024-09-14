5e04b51f6e37af989ca97c4fa00c7fe2c8a1acf6
Devanshi Bilthare <100258554+Devanshi-Bilthare@users.noreply.github.com>
2024-09-14 13:42:10 +0530

    added next tab

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -103,9 +103,16 @@ const Footer = () => {
                       <li>
                         <a href="mailto:info@sahyogplacement.com">info@sahyogplacement.com</a>
                       </li>
-                      <button className="btn text-center bg-white px-4 mt-3" >
-                        <a style={{ color: '#462B34'}} href="https://sahyogplacement.vercel.app/#/">A Login</a>
-                      </button>
+                      <button className="btn text-center bg-white px-4 mt-3">
+  <a
+    style={{ color: '#462B34' }}
+    href="https://sahyogplacement.vercel.app/#/"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    A Login
+  </a>
+</button>
                     </ul>
                   </div>
                   {/* <div className="single-info-box">
